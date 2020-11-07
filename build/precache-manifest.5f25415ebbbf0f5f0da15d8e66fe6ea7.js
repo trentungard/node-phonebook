20438fea631a28c5fe977cc4f69e775c52ecb011
trentungard <trentungard@gmail.com>
2020-11-07 08:31:05 -0500

    test commit

---
+++ build/precache-manifest.5f25415ebbbf0f5f0da15d8e66fe6ea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c997fd601b000c641279d84901d9a102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b4615d131d6f5ec6772",
+    "url": "/static/css/main.ea17cb1e.chunk.css"
+  },
+  {
+    "revision": "fde8ac861ef41d88c8d2",
+    "url": "/static/js/2.9288c642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9288c642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b4615d131d6f5ec6772",
+    "url": "/static/js/main.1d350568.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('logBody', (req, res) => {
     const body = req.body;
